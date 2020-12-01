b21347f8322d07f60f4eb67f6c12963373e8d937
rishonnandi <67328755+rishonnandi@users.noreply.github.com>
2020-12-01 20:08:29 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,41 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.02,
+            'friction':1.0,
+            'density':1.0,
+            'visibility':1.0
+            
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options)
+        this.width = width
+        this.height = height
+        World.add(world, this.body)
+        //this.image = loadImage("sprites/base.png")
+       
+      }
+      display(){
+        var angle = this.body.angle
+        
+        if(this.body.speed < 3){
+          push()
+          translate(this.body.position.x, this.body.position.y)
+          rotate(angle)
+          rectMode(CENTER)
+          fill("red")
+          rect(0, 0, this.width, this.height)
+          pop()
+        }
+       else{
+          World.remove(world, this.body);
+          push();
+          this.visibility = this.visibility -5;
+          pop();
+       }
+      }
+      score(){
+        if(this.visibility < 0 ){
+          score++;
+        }
+      }
+}
